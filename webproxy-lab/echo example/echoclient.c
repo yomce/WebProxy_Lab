13e94b7a03b9fade6c91a82ebf011b280d96cb9a
yomce <kky032911@gmail.com>
2025-05-05 20:30:20 +0900

    echoclient

--- webproxy-lab/echo example/echoclient.c
+++ webproxy-lab/echo example/echoclient.c
@@ -1,6 +1,27 @@
 #include "csapp.h"
 
-int main(int argc, char ** argv)
+int main(int argc, char **argv)
 {
-  /* add code here */
+    int clientfd;
+    char *host, *port, buf[MAXLINE];
+    rio_t rio;
+
+    if (argc != 3) {
+        fprintf(stderr, "usage: %s <host> <port>\n", argv[0]);
+        exit(0);
+    }
+    host = argv[1];
+    port = argv[2];
+
+    clientfd = Open_clientfd(host, port); // 서버에 연결
+    Rio_readinitb(&rio, clientfd);
+
+    while (Fgets(buf, MAXLINE, stdin) != NULL) { // 사용자 입력 받기
+        Rio_writen(clientfd, buf, strlen(buf)); // 서버로 보내기
+        Rio_readlineb(&rio, buf, MAXLINE); // 서버로부터 응답 받기
+        Fputs(buf, stdout); // 화면에 출력
+    }
+
+    Close(clientfd); // 연결 종료
+    exit(0);
 }
