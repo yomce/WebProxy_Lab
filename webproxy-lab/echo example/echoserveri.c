e19722c225d71db9c1c2136f99e826ae1d199846
yomce <kky032911@gmail.com>
2025-05-05 20:29:51 +0900

    echoclient

--- webproxy-lab/echo example/echoserveri.c
+++ webproxy-lab/echo example/echoserveri.c
@@ -2,12 +2,35 @@
 
 void echo(int connfd);
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
-  /* add main code here */
+    int listenfd, connfd;
+    socklen_t clientlen;
+    struct sockaddr_storage clientaddr;
+
+    if (argc != 2) {
+        fprintf(stderr, "usage: %s <port>\n", argv[0]);
+        exit(0);
+    }
+
+    listenfd = Open_listenfd(argv[1]); // 서버 소켓 열기
+    while (1) {
+        clientlen = sizeof(clientaddr);
+        connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen); // 클라이언트 연결 수락
+        echo(connfd); // 에코 처리
+        Close(connfd); // 연결 닫기
+    }
 }
 
 void echo(int connfd)
 {
-  /* add echo code here */
+    size_t n;
+    char buf[MAXLINE];
+    rio_t rio;
+
+    Rio_readinitb(&rio, connfd); // Robust IO 초기화
+    while ((n = Rio_readlineb(&rio, buf, MAXLINE)) != 0) {
+        printf("server received %d bytes\n", (int)n);
+        Rio_writen(connfd, buf, n);
+    }
 }
